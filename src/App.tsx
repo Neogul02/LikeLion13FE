456773dcf94484f85d589c890ee7743cfaf42a69
Choe JinHyeong <wlsgud2414@kangnam.ac.kr>
2024-12-20 21:06:17 +0900

    CI test

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 import { useState } from 'react'
 
+import 침팬지 from './assets/images/침팬지.jpg'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-      <h1>
-        강남대학교 멋쟁이사자처럼 FE 개발현장 🚧
-        <img
-          alt='죽겠어요'
-          src='/public/favicon.ico'
-        />
-      </h1>
+      <h1>강남대학교 멋쟁이사자처럼 FE 개발현장 🚧</h1>
+      <img src={침팬지} />
       <div>
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
       </div>
